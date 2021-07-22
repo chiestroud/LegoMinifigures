95e12fddac86f575802a1d1823a6ec1ff3efe7aa
Chie Stroud <urirunrunkoko@gmail.com>
2021-07-22 18:38:03 -0500

    Add project files.

---
+++ LegoMinifigures/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoMinifigures
+{
+    class Person
+    {
+      
+        public string Head { get; set; }
+        public string Legs { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void SetName (string name)
+        {
+            Console.WriteLine($"Lego person's name is {name}");
+        }
+
+        public void SpecialAttack(string name)
+        {
+            Console.WriteLine($"{name} attacked villages and destroyed everything");
+        }
+
+    }
+}
---
+++ LegoMinifigures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LegoMinifigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            var name = Console.ReadLine();
+            var legoPerson = new Person(name);
+            legoPerson.SetName(name);
+
+            Console.WriteLine("Do you want to see your special abilities? Yes or No?");
+            var answer = Console.ReadLine();
+            if (answer.ToLower() == "yes")
+            {
+                legoPerson.SpecialAttack(name);
+            }
+            else
+            {
+                Console.WriteLine("I think you are too nice.");
+            }
+
+            var newHair = new Clothes();
+            Console.WriteLine("Do you want special clothes?");
+            var clothesAnswer = Console.ReadLine();
+            if (clothesAnswer.ToLower() == "yes")
+            {
+                newHair.getHair(name);
+            } 
+            else
+            {
+                Console.WriteLine($"Goodbye {name}. You are fired.");
+            }
+
+        }
+    }
+}
